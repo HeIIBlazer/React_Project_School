5f023c0605a949f904c1c1592e750d54f83ddb7f
Exorcist <90632640+HeIIBlazer@users.noreply.github.com>
2023-05-15 12:42:52 +0300

    Done with profession edit

--- frontend/src/actions/ProfessionList.js
+++ frontend/src/actions/ProfessionList.js
@@ -24,47 +24,67 @@ export default function ProfessionList() {
     }, [])
 
     return (
-        <Container className='mt-1'>
-            <h2 className='text-center mt-3'>Profession List Manage</h2>
-            <Row>
-                <Col md={{ span: 9, offset: 2 }}>
-                    <p className='text-end'>
-                        <Link to='/addprofession'>
-                            <Button variant='primary' size='sm'>
-                                Add Profession
-                            </Button>
-                        </Link>
-                    </p>
-                    <Table striped>
-                        <thead>
-                            <tr>
-                                <th>No#</th>
-                                <th>Name</th>
-                                <th>Department</th>
-                                <th className='text-center'>Actions</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {profession.map((profession, index) => (
-                                <tr key={profession.id}>
-                                    <td>{index + 1}. #{profession.id}</td>
-                                    <td>{profession.name}</td>
-                                    <td>{profession.department.name}</td>
-                                    <td className='text-center'>
-                                        <Link to={`/detail/${profession.id}`} className='me-1'>
-                                            <Button variant='success' size='sm'>Detail</Button>
-                                        </Link>
-                                        <Link to={`/editprofession/${profession.id}`} className='me-1'>
-                                            <Button variant='primary' size='sm'>Edit</Button>
-                                        </Link>
-                                        <Button onClick={() => deleteProfession(profession.id)} variant='danger' size='sm'>Edit</Button>
-                                    </td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </Table>
-                </Col>
-            </Row>
-        </Container>
-    )
+       <Container className="mt-1">
+          <h2 className="text-center mt-3">Profession List Manage</h2>
+          <Row>
+             <Col md={{ span: 9, offset: 2 }}>
+                <p className="text-end">
+                   <Link to="/addprofession">
+                      <Button variant="primary" size="sm">
+                         Add Profession
+                      </Button>
+                   </Link>
+                </p>
+                <Table striped>
+                   <thead>
+                      <tr>
+                         <th>No#</th>
+                         <th>Name</th>
+                         <th>Department</th>
+                         <th className="text-center">Actions</th>
+                      </tr>
+                   </thead>
+                   <tbody>
+                      {profession.map((profession, index) => (
+                         <tr key={profession.id}>
+                            <td>
+                               {index + 1}. #{profession.id}
+                            </td>
+                            <td>{profession.name}</td>
+                            <td>{profession.department.name}</td>
+                            <td className="text-center">
+                               <Link
+                                  to={`/detail/${profession.id}`}
+                                  className="me-1"
+                               >
+                                  <Button variant="success" size="sm">
+                                     Detail
+                                  </Button>
+                               </Link>
+                               <Link
+                                  to={`/editprofession/${profession.id}`}
+                                  className="me-1"
+                               >
+                                  <Button variant="primary" size="sm">
+                                     Edit
+                                  </Button>
+                               </Link>
+                               <Button
+                                  onClick={() =>
+                                     deleteProfession(profession.id)
+                                  }
+                                  variant="danger"
+                                  size="sm"
+                               >
+                                  delete
+                               </Button>
+                            </td>
+                         </tr>
+                      ))}
+                   </tbody>
+                </Table>
+             </Col>
+          </Row>
+       </Container>
+    );
 }
